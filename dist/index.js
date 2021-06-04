d8e82f3e06750c620a718fcf1fafa6354481fa24
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-06-04 17:55:21 +0900

    types in TS 2

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sayHello = (name, age, gender) => {
-    return `Hello ${name}, you are ${age}, you are ${gender}`;
+    return `Hello ${name}, you are ${age}, you are ${gender}!`;
 };
 console.log(sayHello("Mireu", 27, "male"));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 const sayHello = (name:string, age:number, gender:string): string => {
-    return `Hello ${name}, you are ${age}, you are ${gender}`;
+    return `Hello ${name}, you are ${age}, you are ${gender}!`;
 };
 
 console.log(sayHello("Mireu", 27, "male"));
