108c97b21bc52514d72f36e802f13649d71e4fcb
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-06-04 17:37:29 +0900

    third commit

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Mireu", age = 27, gender = "male";
+const sayHello = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are ${gender}`);
+};
+sayHello(name, age, gender);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,2 +1,11 @@
-console.log("hello");
+const name = "Mireu",
+    age = 27,
+    gender = "male";
 
+const sayHello = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are ${gender}`);
+};
+
+sayHello(name, age, gender);
+
+export {};
